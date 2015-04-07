c933f70ff947918f748de4748289e715df8b9770
Tanvi Mainkar <tmainka@ncsu.edu>
2015-04-07 14:22:21 -0400

    tags map

--- main.js
+++ main.js
@@ -8,10 +8,12 @@ app.get('/', function (req, res) {
 app.listen(8082);
 
 var args=process.argv.slice(2);
-var WINDOW = args[0];
+var WINDOW_SIZE = args[0];
 var K=args[1];
 //Creating a hashmap
 var tagmap = {};
+var tweets = {};
+var tweetcount=0;
 
 //Twitter client
  var client = new twitter({
@@ -21,41 +23,58 @@ var tagmap = {};
   access_token_secret: 'EPiYVc7eE3M9KTTlRZCOXb1ofPwmMebHr6G2kdSBzgOhz'
 });
 
-
-
-client.stream('statuses/filter',{track: 'science'}, function(stream) {
+client.stream('statuses/filter', {track: 'science'},function(stream) {
   stream.on('data', function(tweet) {
     var hashtags= tweet.entities.hashtags;
     console.log(tweet.text);
+ 
+   // tweetcount = (tweetcount+1)%WINDOW_SIZE;
+    
+    //Remove tags from hashmap
+   /* var p=tweets[tweetcount];
+    for(var x in p)
+    {
+      console.log("removing: "+x);
+      tagmap[x]--;
+      if(tagmap[x]==0)
+        delete tagmap[x];
+    }*/
+    tweets[tweetcount]=new Array();
     //Inserting into the hashmap
     for(var i=0; i < hashtags.length;i++){
         key=hashtags[i].text.toUpperCase();
+        //Add hastag to the tweet array
+
+        tweets[tweetcount].push(key);
+        
         if(key in tagmap)
           tagmap[key]++;
         else
-          tagmap[key]=1;
+          tagmap[key]=1;  
     }
-
-
-    //console.log(hashtags[i].text);
-   /* for(var obj in tagmap)
-    {
-      console.log("hash: "+obj + " count :"+tagmap[obj]);
-    }*/
     
+    if(tweets[tweetcount].length===0)
+      {
+        delete tweets[tweetcount];
+       // tweetcount=(tweetcount-1)%WINDOW_SIZE;
+        //tweetcount = tweetcount==0? WINDOW_SIZE: tweetcount;
+      }
+      else tweetcount = (tweetcount+1)%WINDOW_SIZE;
+    console.log(tweets);
+
     var count=sort.sortCount(tagmap);
-    for(var i=0;i<count.length;i++)
+    for(var i=0;i<count.length && i<K;i++)
       //console.log(count[i]);
       console.log("tag: "+count[i][0] +" count: "+count[i][1]);
     });
- 
+    
+    
+
  //---------------------------------------------------
   stream.on('error', function(error) {
     throw error;
   });
 });
-
-
 
 //Sorting the hashmap
 var sort = {
