9f6476ead7f12b9f8984500ec71f4c4a6db5240f
Tanvi Mainkar <tmainka@ncsu.edu>
2015-04-08 00:22:23 -0400

    final

--- main.js
+++ main.js
@@ -1,19 +1,13 @@
-var express = require('express');
 var twitter = require('twitter');
-//express server
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World')
-});
-app.listen(8082);
 
 var args=process.argv.slice(2);
-var WINDOW_SIZE = args[0];
-var K=args[1];
-//Creating a hashmap
-var tagmap = {};
-var tweets = {};
+var WINDOW_SIZE = args[0];    //no. of tweets after which output is written
+var K=args[1];    //no of top hashtags
+
+var tagmap = {};    //holds the hashtags and their counts
+var tweets = {};    //holds tweets and their hashtags
 var tweetcount=0;
+var c=0;
 
 //Twitter client
  var client = new twitter({
@@ -23,30 +17,32 @@ var tweetcount=0;
   access_token_secret: 'EPiYVc7eE3M9KTTlRZCOXb1ofPwmMebHr6G2kdSBzgOhz'
 });
 
-client.stream('statuses/filter', {track: 'science'},function(stream) {
+//stream the hashmaps which contains hashtags and english characters only
+client.stream('statuses/filter', {track: '#', language:'en'},function(stream) {
   stream.on('data', function(tweet) {
     var hashtags= tweet.entities.hashtags;
-    console.log(tweet.text);
- 
-   // tweetcount = (tweetcount+1)%WINDOW_SIZE;
-    
-    //Remove tags from hashmap
-   /* var p=tweets[tweetcount];
-    for(var x in p)
+//    console.log(tweet.text);
+
+//Remove older tweets and thier respective from the map
+   var p=tweets[tweetcount];
+   if(p!=undefined)
     {
-      console.log("removing: "+x);
-      tagmap[x]--;
-      if(tagmap[x]==0)
+      x=p.shift();
+      while(x!=null && x!=undefined)
+      {
+//decrement the count of hashtags. if it reaches 0, remove that tag from map.
+      tagmap[x]= parseInt(tagmap[x])-1;
+      if(parseInt(tagmap[x])==0)
         delete tagmap[x];
-    }*/
+      x=p.shift();
+     }
+  }
+//Inserting new hashtags into the hashmap
     tweets[tweetcount]=new Array();
-    //Inserting into the hashmap
     for(var i=0; i < hashtags.length;i++){
         key=hashtags[i].text.toUpperCase();
         //Add hastag to the tweet array
-
-        tweets[tweetcount].push(key);
-        
+        tweets[tweetcount].push(key);      
         if(key in tagmap)
           tagmap[key]++;
         else
@@ -54,27 +50,28 @@ client.stream('statuses/filter', {track: 'science'},function(stream) {
     }
     
     if(tweets[tweetcount].length===0)
-      {
         delete tweets[tweetcount];
-       // tweetcount=(tweetcount-1)%WINDOW_SIZE;
-        //tweetcount = tweetcount==0? WINDOW_SIZE: tweetcount;
-      }
-      else tweetcount = (tweetcount+1)%WINDOW_SIZE;
-    console.log(tweets);
-
-    var count=sort.sortCount(tagmap);
-    for(var i=0;i<count.length && i<K;i++)
-      //console.log(count[i]);
-      console.log("tag: "+count[i][0] +" count: "+count[i][1]);
+    tweetcount = (tweetcount+1)%WINDOW_SIZE;
+   
+//Print the results after WINDOW_SIZE no. of tweets
+    if(c==parseInt(WINDOW_SIZE)-1)
+    {
+       var count=sort.sortCount(tagmap);
+       console.log("--------------------------");
+        for(var i=0;i<count.length&& i<K;i++)
+            console.log(count[i][0] +" : "+count[i][1]);
+       console.log("--------------------------");
+    }
+    c=(c+1)%WINDOW_SIZE;    
     });
-    
-    
 
  //---------------------------------------------------
   stream.on('error', function(error) {
+   // client.destroy();
     throw error;
   });
 });
+
 
 //Sorting the hashmap
 var sort = {
@@ -83,8 +80,6 @@ var sort = {
     for(var key in obj){
       count.push([key,obj[key]]);
     }
-  //  for(var i=0;i<count.length;i++)
-    //  console.log(count[i]);
     count.sort(function(a,b){return b[1]-a[1]});
     return count;
   },
